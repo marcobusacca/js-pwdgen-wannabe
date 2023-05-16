363ad517d6269d0eb7a133e44f64037c52fa2fff
marcobusacca <marcobusacca01@gmail.com>
2023-05-16 14:45:34 +0200

    [FEAT] inizializzazione variabile password number

--- js/script.js
+++ js/script.js
@@ -6,19 +6,22 @@ let password;
 let password_number;
 
 // INPUT NOME UTENTE
-nome = prompt('Qual è il tuo nome?')
+nome = prompt('Qual è il tuo nome?');
 
 // CONSOL LOG NOME UTENTE
-console.log(nome)
+console.log(nome);
 
 // INPUT COGNOME UTENTE
-cognome = prompt('Qual è il tuo cognome?')
+cognome = prompt('Qual è il tuo cognome?');
 
 // CONSOL LOG COGNOME UTENTE
-console.log(cognome)
+console.log(cognome);
 
 // INPUT COLORE PREFERITO UTENTE
-colore_preferito = prompt('Qual è il tuo colore preferito?')
+colore_preferito = prompt('Qual è il tuo colore preferito?');
 
 // CONSOL LOG COLORE PREFERITO UTENTE
-console.log(colore_preferito)
+console.log(colore_preferito);
+
+// PASSWORD NUMBER
+password_number = 21;

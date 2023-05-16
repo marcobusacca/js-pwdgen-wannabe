c02b900ce7c11d5729f4acb29a6e12442469ad62
marcobusacca <marcobusacca01@gmail.com>
2023-05-16 14:41:39 +0200

    [FEAT] input nome utente e aggiunta commenti

--- js/script.js
+++ js/script.js
@@ -1,5 +1,12 @@
+// DICHIARAZIONE VARIABILI
 let nome;
 let cognome;
 let colore_preferito;
 let password;
 let password_number;
+
+// INPUT NOME UTENTE
+nome = prompt('Qual è il tuo nome?')
+
+// CONSOL LOG NOME UTENTE
+console.log(nome)

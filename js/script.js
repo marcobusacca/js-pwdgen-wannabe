f8db386296a2178e6d905dc52cd08965f9a34ad4
marcobusacca <marcobusacca01@gmail.com>
2023-05-17 09:37:43 +0200

    [REFACTOR] Esercizio Completato

--- js/script.js
+++ js/script.js
@@ -7,32 +7,22 @@ let password;
 
 // INPUT NOME UTENTE
 nome = prompt('Qual è il tuo nome?');
-
-// CONSOL LOG NOME UTENTE
 console.log(nome);
 
 // INPUT COGNOME UTENTE
 cognome = prompt('Qual è il tuo cognome?');
-
-// CONSOL LOG COGNOME UTENTE
 console.log(cognome);
 
 // INPUT COLORE PREFERITO UTENTE
 colore_preferito = prompt('Qual è il tuo colore preferito?');
-
-// CONSOL LOG COLORE PREFERITO UTENTE
 console.log(colore_preferito);
 
 // PASSWORD NUMBER
 password_number = 21;
-
-// CONSOL LOG PASSWORD NUMBER
 console.log(password_number);
 
 // PASSWORD GENERATOR
 password = nome + cognome + colore_preferito + password_number;
-
-// CONSOL LOG PASSWORD GENERATOR
 console.log(password);
 
 // PASSWORD OUTPUT ALERT PER L'UTENTE
